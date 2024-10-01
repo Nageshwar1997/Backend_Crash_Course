f7e4179317a310fc07c0e240eb154acbfee12a6e
Nageshwar <nageshpawarpatil@gmail.com>
2024-10-01 15:17:35 +0530

    CJS & MJS Added

---
+++ Day-01-Intro-To_Backend-And-NodeJS/Examples/mjs/index.mjs
@@ -0,0 +1,23 @@
+
+
+// const args = process.argv.slice(2);
+// const operation = args[0];
+// const num1 = Number(args[1]);
+// const num2 = Number(args[2]);
+
+import printName from "./printName.mjs";
+import sub from "./sub.mjs";
+import sum from "./sum.mjs";
+
+// console.log(process)
+console.log(process.argv);
+/**
+[
+  'C:\\Program Files\\nodejs\\node.exe',
+  'd:\\GitHub Repos\\Backend_Crash_Course\\Day-01-Intro-To_Backend-And-NodeJS\\Examples\\Example-01\\index.js'
+]
+*/
+
+console.log(sum(1, 2));
+console.log(sub(5, 2));
+console.log(printName("Nageshwar"));
